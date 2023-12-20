33761f95bf4f4d38cce57a0ab7fae6e51a2e65b3
ptak18 <ptakyie@gmail.com>
2023-12-20 18:31:14 +0000

    adding comments on progress and to dos

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,44 +1,37 @@
 var score = 90;
 var currentQuestion = 0;
+var timerInterval ;
 
-console.log(myQuestions[currentQuestion]);
 
-// CRAETE A CODE QUIZ THAT COMTAINS:
-// A start button that when clicked a timer
-// starts and the first question appears.
 
-// Questions conatins buttons for each answer.
+// get elements from HTML
 
-// When the answer is clicked,the next question appears
 
-// If the answer clicked was imcorrect then subtarct time from the
-// clock
-
-// the quiz should end when all the questions are answered or the timer reaches 0.
-
-// when the game ends, it should display their score and give
-// the user the ability to save their score
 var timeElement = document.getElementById("time")
 
 var questionsElement = document.getElementById("questions");
 var questionTitleEl = document.getElementById("question-title");
-questionTitleEl.innerText = myQuestions[currentQuestion].question;
 
 var choicesEl = document.getElementById("choices");
 var answersUl = document.getElementById("answers");
 var answerOne = document.getElementById("answerA");
 var answerTwo = document.getElementById("answerB");
 var answerThree = document.getElementById("answerC");
+var startScreen = document.getElementById("start-screen");
+var startButton = document.getElementById("start");
 
+
+// setting question to be the current question 
+questionTitleEl.innerText = myQuestions[currentQuestion].question;
 answerOne.innerText = myQuestions[currentQuestion].answers.A;
 answerTwo.innerText = myQuestions[currentQuestion].answers.B;
 answerThree.innerText = myQuestions[currentQuestion].answers.C;
-
+// adding eventlistner to each answer
 answerOne.addEventListener("click", checkAnswer);
 answerTwo.addEventListener("click", checkAnswer);
 answerThree.addEventListener("click", checkAnswer);
-
-var startScreen = document.getElementById("start-screen");
+// adds event listner to start button 
+startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
   questionsElement.classList.remove("hide");
@@ -46,8 +39,6 @@ function startQuiz() {
   startTimer();
 
 }
-var startButton = document.getElementById("start");
-startButton.addEventListener("click", startQuiz);
 
 function displayNextQuestion() {
     console.log (score)
@@ -59,7 +50,8 @@ function displayNextQuestion() {
 }
 
 function endQuiz() {
-    
+    clearInterval(timerInterval)
+   console.log(score) 
 }
 
 function updateTime() {
@@ -71,13 +63,13 @@ if (score <1){
 }
 
 function startTimer() {
-    setInterval( 
+    timerInterval = setInterval( 
     updateTime    
     , 1000);
 }
 
 function checkAnswer() {
-  console.log(event.target.id[6]);
+ // gets answers letter 
   var answer = event.target.id[6];
   var correctAnswer = myQuestions[currentQuestion].correctAnswer;
   if (answer !== correctAnswer) {
@@ -92,3 +84,6 @@ function checkAnswer() {
 
 }
 
+// TO DO : 
+// add local storage to save highscores
+// use local storage to get highscores.html
